77faea2a9fc5815bf1d912d3c98a9f0138bb187d
Richard FAVREAU <rfavreau@rfd.ovh>
2024-05-26 09:05:47 +0200

    Fix CI action

--- acceptance/data.go
+++ acceptance/data.go
@@ -5,12 +5,9 @@ package acceptance
 
 import (
 	"fmt"
-	"math"
 	"math/rand"
 	"os"
 	"strconv"
-	"strings"
-	"time"
 )
 
 const (
@@ -49,19 +46,19 @@ func BuildTestData(resourceType string, resourceLabel string) TestData {
 	return testData
 }
 
-func (TestData) RandomInteger() int {
-	return randTimeInt()
+func (td *TestData) RandomInteger() int {
+	return td.RandomIntOfLength(999999)
 }
 
-func (TestData) RandomIntegerString() string {
-	return strconv.Itoa(randTimeInt())
+func (td *TestData) RandomIntegerString() string {
+	return strconv.Itoa(td.RandomInteger())
 }
 
-func (TestData) RandomString() string {
+func (td *TestData) RandomString() string {
 	return randString(5)
 }
 
-func (TestData) BuildArrayString(array []string) string {
+func (td *TestData) BuildArrayString(array []string) string {
 	var str = ""
 
 	for i := 0; i < len(array); i++ {
@@ -74,32 +71,8 @@ func (TestData) BuildArrayString(array []string) string {
 	return fmt.Sprintf("[%s]", str)
 }
 
-// RandomIntOfLength is a random 8 to 18 digit integer which is unique to this test case
 func (td *TestData) RandomIntOfLength(len int) int {
-	// len should not be
-	//  - greater then 18, longest a int can represent
-	//  - less then 8, as that gives us YYMMDDRR
-	if 8 > len || len > 18 {
-		panic("Invalid Test: RandomIntOfLength: len is not between 8 or 18 inclusive")
-	}
-
-	// 18 - just return the int
-	if len >= 18 {
-		return td.RandomInteger()
-	}
-
-	// 16-17 just strip off the last 1-2 digits
-	if len >= 16 {
-		return td.RandomInteger() / int(math.Pow10(18-len))
-	}
-
-	// 8-15 keep len - 2 digits and add 2 characters of randomness on
-	s := strconv.Itoa(td.RandomInteger())
-	r := s[16:18]
-	v := s[0 : len-2]
-	i, _ := strconv.Atoi(v + r)
-
-	return i
+	return rand.Intn(len)
 }
 
 // RandomStringOfLength is a random 1 to 1024 character string which is unique to this test case
@@ -125,22 +98,4 @@ func randStringFromCharSet(strlen int, charSet string) string {
 		result[i] = charSet[rand.Intn(len(charSet))]
 	}
 	return string(result)
-}
-
-func randTimeInt() int {
-	// acctest.RantInt() returns a value of size:
-	// 000000000000000000
-	// YYMMddHHmmsshhRRRR
-
-	// go format: 2006-01-02 15:04:05.00
-
-	timeStr := strings.Replace(time.Now().Local().Format("060102150405.00"), ".", "", 1) // no way to not have a .?
-	postfix := randStringFromCharSet(4, "0123456789")
-
-	i, err := strconv.Atoi(timeStr + postfix)
-	if err != nil {
-		panic(err)
-	}
-
-	return i
 }
