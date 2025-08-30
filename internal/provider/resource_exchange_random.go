59ab88688e97c478d9ffdcff3be4ae0497d896c8
Richard FAVREAU <rfavreau@rfd.ovh>
2025-08-30 15:21:43 +0200

    Add ressource and datasource

---
+++ internal/provider/resource_exchange_random.go
@@ -0,0 +1,33 @@
+package provider
+
+import (
+	rabbithole "github.com/michaelklishin/rabbit-hole/v3"
+	"github.com/rfd59/terraform-provider-rabbitmq/internal/provider/core"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceExchangeRandom() *schema.Resource {
+	return &schema.Resource{
+		Description: "Exchange --- The `rabbitmq_exchange_random` resource creates and manages an _exchange_ of type 'x-random'.",
+		Create:      CreateExchangeRandom,
+		Read:        ReadExchangeRandom,
+		Delete:      DeleteExchangeRandom,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: core.ResourceExchange(),
+	}
+}
+
+func CreateExchangeRandom(d *schema.ResourceData, meta interface{}) error {
+	return core.CreateExchange(d, meta.(*rabbithole.Client))
+}
+
+func ReadExchangeRandom(d *schema.ResourceData, meta interface{}) error {
+	return core.ReadExchange(d, meta.(*rabbithole.Client))
+}
+
+func DeleteExchangeRandom(d *schema.ResourceData, meta interface{}) error {
+	return core.DeleteExchange(d, meta.(*rabbithole.Client))
+}
